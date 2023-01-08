d4c1d125578020363d2503f915a596bdd54521c6
retyny <anko-style@mail.ru>
2023-01-09 01:52:07 +0200

    new_09_01_2023

--- script/script.js
+++ script/script.js
@@ -1,76 +1,88 @@
 
 const storageVal = localStorage.getItem("customers");
-const database = storageVal?JSON.parse(storageVal):[];
+const database = storageVal?JSON.parse(storageVal):[]
 
-    for (let i = 0; i < database.length; i++) {
-        if(database.length >= 0) {
-            console.log(database);
+        saveTravel = () => {
+            location.reload();
+            const customer = {
+
+                fromCity:document.getElementById("city_1").value,
+                toCity:document.getElementById("city_2").value,
+                budget:document.getElementById("budget_").value,
+                dateStart:document.getElementById("date_start").value,
+                dateEnd:document.getElementById("date_end").value,
+                persons:document.getElementById("persons_").value,
+                methodTravel:document.getElementById("method_travel").value,
+            };
+                if (customer.fromCity === "" || customer.toCity === ""
+                    || customer.fromCity === " " || customer.toCity === " "
+                    || customer.fromCity === "  " || customer.toCity === "  "
+                    || customer.fromCity === "   " || customer.toCity === "   ") {
+                    alert('Заполните важные поля!');
+                    return 0;
+                } else {
+                    // Перевод customer в стринг и пуш в базу данных
+                    database.push(JSON.stringify(customer));
+                    // Сохранение всех билетов в localStorage
+                    localStorage.setItem('customers', JSON.stringify(database));
+                    render();
+                }
+        }
+
+
+        render = () => {
+            database.map((value, index) => {
+            if(value) {
             const rightUl = document.getElementById("right_ul");
             const firstLi = rightUl.getElementsByTagName('LI')[0];
             // Новый элемент
-            const newRightUlElement = document.createElement('LI')
-            newRightUlElement.className = 'elementHistory';
+            const newRightElement = document.createElement('LI')
+            newRightElement.className = 'elementHistory';
             //Вставляем элемент
-            newRightUlElement.innerHTML = `
-<h3>From ${JSON.parse(database[i]).fromCity} to ${JSON.parse(database[i]).toCity}</h3> <h4>Expected budget:${JSON.parse(database[i]).budget} ILS</h4>
-<h4>${JSON.parse(database[i]).dateStart} - ${JSON.parse(database[i]).dateEnd} | ${JSON.parse(database[i]).persons} Persons |${JSON.parse(database[i]).methodTravel} </h4>
-`
-            rightUl.insertBefore(newRightUlElement, firstLi);
-            console.log(database[i]);
+            newRightElement.innerHTML =
+                `
+                <div class="item2">
+            <h3>From ${JSON.parse(value).fromCity} to ${JSON.parse(value).toCity}</h3> <h4>Expected budget:${JSON.parse(value).budget} ILS</h4>
+            <h4>${JSON.parse(value).dateStart} - ${JSON.parse(value).dateEnd} | ${JSON.parse(value).persons} Persons |${JSON.parse(value).methodTravel} </h4>
+                </div>    
+                <li class="item1" id="${index}">
+                    <button class="delete" id="${index}">del</button>
+                    <button class="edit" id="${index}">2</button>
+                    <button class="details" id="${index}">3</button>
+                </li>    
+                `
+            rightUl.insertBefore(newRightElement, firstLi);
+            setUpIcons();
         }
-    }
-
-
-    function customerCreate() {
-    const customer = {
-        fromCity:document.getElementById("city_1").value,
-        toCity:document.getElementById("city_2").value,
-        budget:document.getElementById("budget_").value,
-        dateStart:document.getElementById("date_start").value,
-        dateEnd:document.getElementById("date_end").value,
-        persons:document.getElementById("persons_").value,
-        methodTravel:document.getElementById("method_travel").value,
-    };
-    return customer;
+    });
 }
 
 
-function divCreate() {
-        customer = customerCreate();
-        if (customer.fromCity === "" || customer.toCity === ""
-            || customer.fromCity === " " || customer.toCity === " "
-            || customer.fromCity === "  " || customer.toCity === "  "
-            || customer.fromCity === "   " || customer.toCity === "   ") {
-            alert('Заполните важные поля!');
-            return 0;
-        } else {
-            // Родительский элемент
-            const rightUl = document.getElementById("right_ul");
-            const firstLi = rightUl.getElementsByTagName('LI')[0];
-            // Новый элемент
-            const newRightUlElement = document.createElement('LI')
-            newRightUlElement.className = 'elementHistory';
-            //Вставляем элемент
-            newRightUlElement.innerHTML = `
-<h3>From ${customer.fromCity} to ${customer.toCity}</h3> <h4>Expected budget:${customer.budget} ILS</h4>
-<h4>${customer.dateStart} - ${customer.dateEnd} | ${customer.persons} Persons |${customer.methodTravel} </h4>
-`
-            rightUl.insertBefore(newRightUlElement, firstLi);
-        }
 
+    const setUpIcons = () => {
+    document.querySelectorAll('.delete').forEach(value => {
+        value.addEventListener('click', deleteTravel);
+    });
+    document.querySelectorAll('.edit').forEach(value => {
+        value.addEventListener('click', editTravel);
+    });
+    document.querySelectorAll('.details').forEach(value => {
+        value.addEventListener('click', showTravelDetails);
+    })
 }
-
-function saveTravel() {
-    if (divCreate() === 0){
-        return 0;
-        } else {
-        // Перевод customer в стринг и пуш в базу данных
-        database.push(JSON.stringify(customer = customerCreate()));
-        // Сохранение всех билетов в localStorage
+    const deleteTravel = value => {
+    const id = Number(value.target.getAttribute('id'));
+        database.splice(id, 1);
         localStorage.setItem('customers', JSON.stringify(database));
-        }
-    }
+        location.reload();
+}
+    const editTravel = value =>  {
+    const id = Number(value.target.getAttribute('id'));
+    console.log("edit - "+id);
+}
+    const showTravelDetails = value => {
+    const id = Number(value.target.getAttribute('id'));
+    console.log("details - "+id);
+}
 
-
-
-
+render();
