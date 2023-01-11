704b65758143b95fc91ffa1b07d39263de38e268
retyny <anko-style@mail.ru>
2023-01-11 10:20:09 +0200

    Hw_18_
    Hw_18_

--- script/script.js
+++ script/script.js
@@ -2,6 +2,56 @@
 const storageVal = localStorage.getItem("customers");
 const database = storageVal?JSON.parse(storageVal):[]
 
+        saveTravelModal = () => {
+            const customer_modal = {
+                fromCity:document.getElementById("city_1_modal").value,
+                toCity:document.getElementById("city_2_modal").value,
+                budget:document.getElementById("budget_modal").value,
+                dateStart:document.getElementById("date_start").value,
+                dateEnd:document.getElementById("date_end_modal").value,
+                persons:document.getElementById("persons_modal").value,
+                methodTravel:document.getElementById("method_travel_modal").value,
+            };
+                // Перевод customer в стринг и пуш в базу данных
+                database.push(JSON.stringify(customer_modal));
+                // Сохранение всех билетов в localStorage
+                localStorage.setItem('customers', JSON.stringify(database));
+            location.reload();
+        }
+
+        saveNewModal = () => {
+        const modal = document.createElement('div');
+        modal.classList.add('wrapper_modal');
+        modal.insertAdjacentHTML('afterbegin',`
+            <div class="modal">
+            <span class="modal_close">&times;</span>
+            <h1> Edit:</h1>
+            <input type="text" maxlength="16" class="place_modal"  id="city_1_modal" placeholder="from City"> <br>
+            <input type="text" maxlength="16" class="place_modal" id="city_2_modal" placeholder="to City"> <br>
+            <input type="text" maxlength="16" class="place_modal" id="budget_modal" placeholder="Budget">
+            <h3>Date start:</h3>
+            <input  type="date" class="place_modal" id="date_start_modal">
+            <h3>Date end:</h3>
+            <input  type="date" class="place_modal" id="date_end_modal">
+            <h3>Persons:</h3>
+            <input type="number" max="12" class="place_modal" id="persons_modal">
+            <h3>Maint transfer type</h3>
+            <select id="method_travel_modal" class="place_modal">
+                <option>--</option>
+                <option>Private car</option>
+                <option>Railway</option>
+                <option>Fly</option>
+                <option>Rent car</option>
+                <option>Taxi</option>
+                <option>Bus</option>
+            </select> <br>
+            <button id="save_travel_modal" class="btn_modal" onclick="saveTravelModal()">Save Travel</button>
+        </div>
+`)
+            document.body.appendChild(modal);
+            return modal;
+        }
+
         saveTravel = () => {
             location.reload();
             const customer = {
@@ -47,7 +97,7 @@ const database = storageVal?JSON.parse(storageVal):[]
                 </div>    
                 <li class="item1" id="${index}">
                     <button class="delete" id="${index}">del</button>
-                    <button class="edit" id="${index}">2</button>
+                    <button class="edit" id="${index}">edit</button>
                     <button class="details" id="${index}">3</button>
                 </li>    
                 `
@@ -78,7 +128,8 @@ const database = storageVal?JSON.parse(storageVal):[]
 }
     const editTravel = value =>  {
     const id = Number(value.target.getAttribute('id'));
-    console.log("edit - "+id);
+    saveNewModal();
+    console.log(saveNewModal());
 }
     const showTravelDetails = value => {
     const id = Number(value.target.getAttribute('id'));
@@ -86,3 +137,4 @@ const database = storageVal?JSON.parse(storageVal):[]
 }
 
 render();
+
