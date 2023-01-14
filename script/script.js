1679567913a8e80c824ea4948e4853ce1754c516
retyny <anko-style@mail.ru>
2023-01-14 08:45:11 +0200

    1
    1

--- script/script.js
+++ script/script.js
@@ -2,60 +2,10 @@
 const storageVal = localStorage.getItem("customers");
 const database = storageVal?JSON.parse(storageVal):[]
 
-        saveTravelModal = () => {
-            const customer_modal = {
-                fromCity:document.getElementById("city_1_modal").value,
-                toCity:document.getElementById("city_2_modal").value,
-                budget:document.getElementById("budget_modal").value,
-                dateStart:document.getElementById("date_start").value,
-                dateEnd:document.getElementById("date_end_modal").value,
-                persons:document.getElementById("persons_modal").value,
-                methodTravel:document.getElementById("method_travel_modal").value,
-            };
-                // Перевод customer в стринг и пуш в базу данных
-                database.push(JSON.stringify(customer_modal));
-                // Сохранение всех билетов в localStorage
-                localStorage.setItem('customers', JSON.stringify(database));
-            location.reload();
-        }
-
-        saveNewModal = () => {
-        const modal = document.createElement('div');
-        modal.classList.add('wrapper_modal');
-        modal.insertAdjacentHTML('afterbegin',`
-            <div class="modal">
-            <span class="modal_close">&times;</span>
-            <h1> Edit:</h1>
-            <input type="text" maxlength="16" class="place_modal"  id="city_1_modal" placeholder="from City"> <br>
-            <input type="text" maxlength="16" class="place_modal" id="city_2_modal" placeholder="to City"> <br>
-            <input type="text" maxlength="16" class="place_modal" id="budget_modal" placeholder="Budget">
-            <h3>Date start:</h3>
-            <input  type="date" class="place_modal" id="date_start_modal">
-            <h3>Date end:</h3>
-            <input  type="date" class="place_modal" id="date_end_modal">
-            <h3>Persons:</h3>
-            <input type="number" max="12" class="place_modal" id="persons_modal">
-            <h3>Maint transfer type</h3>
-            <select id="method_travel_modal" class="place_modal">
-                <option>--</option>
-                <option>Private car</option>
-                <option>Railway</option>
-                <option>Fly</option>
-                <option>Rent car</option>
-                <option>Taxi</option>
-                <option>Bus</option>
-            </select> <br>
-            <button id="save_travel_modal" class="btn_modal" onclick="saveTravelModal()">Save Travel</button>
-        </div>
-`)
-            document.body.appendChild(modal);
-            return modal;
-        }
 
         saveTravel = () => {
             location.reload();
             const customer = {
-
                 fromCity:document.getElementById("city_1").value,
                 toCity:document.getElementById("city_2").value,
                 budget:document.getElementById("budget_").value,
@@ -78,8 +28,6 @@ const database = storageVal?JSON.parse(storageVal):[]
                     render();
                 }
         }
-
-
         render = () => {
             database.map((value, index) => {
             if(value) {
@@ -106,9 +54,6 @@ const database = storageVal?JSON.parse(storageVal):[]
         }
     });
 }
-
-
-
     const setUpIcons = () => {
     document.querySelectorAll('.delete').forEach(value => {
         value.addEventListener('click', deleteTravel);
@@ -128,8 +73,35 @@ const database = storageVal?JSON.parse(storageVal):[]
 }
     const editTravel = value =>  {
     const id = Number(value.target.getAttribute('id'));
-    saveNewModal();
-    console.log(saveNewModal());
+    console.log(id);
+        const wrapperModal = document.getElementById('wrapper_modal');
+        wrapperModal.classList.remove('hide');
+        document.getElementById('modal_close')
+            .addEventListener('click', () => {
+                wrapperModal.classList.add('hide');
+            });
+       // document.getElementById('wrapper_modal')
+        //    .addEventListener('click', () => {
+         //       wrapperModal.classList.add('hide');
+          //  });
+        document.getElementById('save_travel_modal')
+            .addEventListener('click', () => {
+                const customerNew = {
+                    fromCity:document.getElementById("city_1_modal").value,
+                    toCity:document.getElementById("city_2_modal").value,
+                    budget:document.getElementById("budget_modal").value,
+                    dateStart:document.getElementById("date_start_modal").value,
+                    dateEnd:document.getElementById("date_end_modal").value,
+                    persons:document.getElementById("persons_modal").value,
+                    methodTravel:document.getElementById("method_travel_modal").value,
+                };
+                database.splice(id, 1);
+                database[id] = JSON.stringify(customerNew);
+                localStorage.setItem('customers', JSON.stringify(database));
+                wrapperModal.classList.add('hide');
+                render();
+                location.reload();
+        });
 }
     const showTravelDetails = value => {
     const id = Number(value.target.getAttribute('id'));
@@ -137,4 +109,7 @@ const database = storageVal?JSON.parse(storageVal):[]
 }
 
 render();
+
+
+
 
